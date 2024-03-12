964555408843a17fd6927442af375546c1cc1c61
KateSt <kateystetsenko@gmail.com>
2024-03-12 14:01:02 +0200

    add: instance

--- src/Store/features/user/UsersSlice.ts
+++ src/Store/features/user/UsersSlice.ts
@@ -1,31 +1,37 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { User } from 'Types/User'
-import { AppDispatch, RootState } from '../../store'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from 'Types/User';
+import { AppDispatch, RootState } from '../../store';
 
 export interface UserState {
-    user: User
+  user: User,
+  accessToken: string | null;
 }
 
 const initialState: UserState = {
-    user: {} as User,
-}
+  user: {} as User,
+  accessToken: null,
+};
 
 export const UsersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {
-        setUser: (state, action: PayloadAction<User>) => {
-            state.user = action.payload
-        },
+  name: 'users',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
     },
-})
+    setToken: (state, action) => {
+      state.accessToken = action.payload;
+    },
+  },
+});
 
-export const { setUser } = UsersSlice.actions
+export const { setUser, setToken } = UsersSlice.actions;
 
-export const selectUser = (state: RootState) => state.users.user
+export const selectUser = (state: RootState) => state.users.user;
+export const selectToken = (state: RootState) => state.users.accessToken;
 
 export const setUserAsync = (user: User) => async (dispatch: AppDispatch) => {
-    // await fetch().then().catch();
-    dispatch(setUser(user))
-}
-export default UsersSlice.reducer
+  // await fetch().then().catch();
+  dispatch(setUser(user));
+};
+export default UsersSlice.reducer;
