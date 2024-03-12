513971f789cfd7b911760336b5f807664db6ca79
KateSt <kateystetsenko@gmail.com>
2024-03-12 13:24:07 +0200

    add: post

--- src/Pages/user/authorization/AuthorizationPage.tsx
+++ src/Pages/user/authorization/AuthorizationPage.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Button, TextField } from '@mui/material';
 
 const AuthorizationPage = () => {
-    return <div>auth</div>
-}
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const handelValueChange = () => {
+    axios.post('/auth/login/', {email,password}).then((data) => data.data)
+      .then((el) => console.log(el))
+      .catch((err) => console.log('Don`t correct input', err));
+  };
 
-export default AuthorizationPage
+  return (
+    <div>
+      <TextField id="outlined-required" label="email" variant="outlined" value={email}
+                 onChange={e => setEmail(e.target.value)} />
+      <TextField id="outlined-password-input" label="password" autoComplete="current-password" type="password"
+                 value={password}
+                 onChange={e => setPassword(e.target.value)} />
+      <Button color="secondary" onClick={handelValueChange}>Secondary</Button>
+    </div>
+  );
+};
+
+export default AuthorizationPage;
