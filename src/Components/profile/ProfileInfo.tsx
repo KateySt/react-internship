a1908d5dd25381f162b3a846c0a2f4a6151a4cc6
KateSt <kateystetsenko@gmail.com>
2024-03-20 13:47:49 +0200

    refactor: user

--- src/Components/profile/ProfileInfo.tsx
+++ src/Components/profile/ProfileInfo.tsx
@@ -5,39 +5,39 @@ import 'react-phone-input-2/lib/material.css';
 import StyleButton from '../button/StyleButton';
 
 interface ProfileInfoProps {
-  profile: User;
+  user: User;
   isEditable: boolean;
   onEditClick: () => void;
 }
 
-const ProfileInfo: React.FC<ProfileInfoProps> = ({ profile, isEditable, onEditClick }) => {
+const ProfileInfo: React.FC<ProfileInfoProps> = ({ user, isEditable, onEditClick }) => {
   return (
     <>
-      {!!profile.user_avatar ? (
-        <Avatar alt={profile.user_firstname} src={profile.user_avatar}
+      {!!user.user_avatar ? (
+        <Avatar alt={user.user_firstname} src={user.user_avatar}
                 sx={{ width: 120, height: 120, margin: 'auto' }} />
       ) : (
         <Avatar sx={{ width: 120, height: 120, margin: 'auto' }} />
       )}
       <Typography variant="h5" gutterBottom>
-        {`${profile.user_firstname} ${profile.user_lastname}`}
+        {`${user.user_firstname} ${user.user_lastname}`}
       </Typography>
       <Typography variant="body1" color="textSecondary">
-        e-mail: {profile.user_email}
+        e-mail: {user.user_email}
       </Typography>
       <Typography variant="body1" color="textSecondary">
-        city: {profile.user_city}
+        city: {user.user_city}
       </Typography>
       <Typography variant="body1" color="textSecondary">
-        phone: {profile.user_phone}
+        phone: {user.user_phone}
       </Typography>
       <Typography variant="body1" color="textSecondary">
-        status: {profile.user_status}
+        status: {user.user_status}
       </Typography>
       <Typography variant="body1" color="textSecondary">
         links:
         <List>
-          {profile.user_links && profile.user_links.map((link, index) => (
+          {user.user_links && user.user_links.map((link, index) => (
             <ListItem key={index}>
               <ListItemText primary={link} />
             </ListItem>
